f6b748c18dfe1f045f793e86a6ece52b71b7f93c
Gelfs <91386069+Gelfs@users.noreply.github.com>
2022-03-13 21:52:28 -0500

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,19 @@
+
+
+function result() {
+	  var c = 0;
+		var q1=document.quiz.question1.value;
+    var q2=document.quiz.question2.value;
+    var q3=document.quiz.question3.value;
+		if (q1=="Minecraft") {
+      c++
+    }
+    if (q2=="Cory") {
+      c++
+
+    }
+    if (q3=="Coke") {
+      c++
+    }
+    document.write("You got "+c+"/3 correct!")
+}
