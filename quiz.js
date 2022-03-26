19213f1cd6e5ea7f29303d16280bef8567e2e697
Gelfs <91386069+Gelfs@users.noreply.github.com>
2022-03-26 17:49:08 -0500

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,11 +1,11 @@
 
 
 function result() {
-	  var c = 0;
-		var q1=document.quiz.question1.value;
+    var c = 0;
+    var q1=document.quiz.question1.value;
     var q2=document.quiz.question2.value;
     var q3=document.quiz.question3.value;
-		if (q1=="Minecraft") {
+    if (q1=="Minecraft") {
       c++
     }
     if (q2=="Cory") {
@@ -15,5 +15,16 @@ function result() {
     if (q3=="Coke") {
       c++
     }
-    document.write("You got "+c+"/3 correct!")
+    if (c==3) {
+      document.write("<h2 class='heading'>Thank you for taking my quiz!</h2><br>" + "<p class='TheGrade'>You got a 100% on this QUIZ!!!</p><br> <p class='TheGrade'>Good Job studying!</p>" +"<br><br>This was made out of boredom by Gelfs" + "<link rel='stylesheet' href='quiz.css'>")
+    }
+    if (c==2) {
+      document.write("<h2 class='heading'>Thank you for taking my quiz!</h2><br>" + "<p class='TheGrade'>You got a 66% on this QUIZ!!!</p><br> <p class='TheGrade'>I believe you can better!</p>" +"<br><br>This was made out of boredom by Gelfs" + "<link rel='stylesheet' href='quiz.css'>")
+    }
+    if (c==1) {
+      document.write("<h2 class='heading'>Thank you for taking my quiz!</h2><br>" + "<p class='TheGrade'>You got a 33% on this QUIZ!!!</p><br> <p class='TheGrade'>I believe you can better!</p>" +"<br><br>This was made out of boredom by Gelfs" + "<link rel='stylesheet' href='quiz.css'>")
+    }
+    if (c==1) {
+      document.write("<h2 class='heading'>Thank you for taking my quiz!</h2><br>" + "<p class='TheGrade'>You got a 0% on this QUIZ!!!</p><br> <p class='TheGrade'>I believe you can better!</p>" +"<br><br>This was made out of boredom by Gelfs" + "<link rel='stylesheet' href='quiz.css'>")
+    }
 }
